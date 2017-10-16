4ae609cda60e1c33864be167c090d70af8605214
Usuario <Usuario@192.168.60.47>
2017-10-16 12:01:45 +0200

    clase CD

--- src/CD.java
+++ src/CD.java
@@ -1,9 +1,46 @@
 
 public class CD extends Articulo {
-	private int id;
-	private String titulo;
+	
+	
 	private String cantante;
 	private String discografia;
+	
+	
+public CD(int id, String ti, int stock,   String cantante, String discografia) {
+		super(id, ti, stock);
+			
+		this.cantante = cantante;
+		this.discografia = discografia;
+	}
+
+public String getCantante() {
+	return cantante;
+}
+
+
+public void setCantante(String cantante) {
+	this.cantante = cantante;
+}
+
+
+public String getDiscografia() {
+	return discografia;
+}
+
+
+public void setDiscografia(String discografia) {
+	this.discografia = discografia;
+}
+
+public void borrar() {
+	
+}
+public void actualizar() {
+	
+}
+public void anadir() {
+	
+}
 	
 
 	
