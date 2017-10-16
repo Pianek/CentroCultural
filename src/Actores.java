ee4e9fb3826321240d63e2ae4131bfbede73241d
Usuario <Usuario@192.168.60.9>
2017-10-16 11:20:29 +0200

    Actores y Articulo creadas

---
+++ src/Actores.java
@@ -0,0 +1,38 @@
+
+public class Actores {
+	public int id_actor;
+	public String nombre;
+	
+	
+	public Actores(int id, String nom) {
+		this.id_actor=id;
+		this.nombre=nom;
+	}
+	
+	
+	
+	public void actualizar() {
+		
+	}
+	
+	public void crear() {
+		
+	}
+	
+	public int getId_actor() {
+		return id_actor;
+	}
+	
+	public void setId_actor(int id_actor) {
+		this.id_actor = id_actor;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
---
+++ src/Articulo.java
@@ -0,0 +1,33 @@
+public class Articulo {
+	private int id_articulo;
+	private String titulo;
+	private int stock;
+	
+	
+public Articulo(int id, String ti, int stock) {
+	this.id_articulo=id;
+	this.titulo=ti;
+	this.stock=stock;
+}
+	
+	public int getId_articulo() {
+		return id_articulo;
+	}
+	public void setId_articulo(int id_articulo) {
+		this.id_articulo = id_articulo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	
+}
