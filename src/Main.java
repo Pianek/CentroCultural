e144f43f543929cdd7ed5df39fdb73cada1ff33d
Usuario <Usuario@192.168.60.47>
2017-10-25 12:30:31 +0200

    FrontArticulo

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		//Conexion conn = new Conexion();
 //		FrontAdmin admin = new FrontAdmin();
-//		FrontArticulo articulo = new FrontArticulo();
-		FrontAlquilarCD alquilar = new FrontAlquilarCD();
+
+		FrontArticulo articulo = new FrontArticulo();
+	//	FrontAlquilarCD alquilar = new FrontAlquilarCD();
+		
 		
 		
 
