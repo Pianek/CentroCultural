a0fa3283fc3b6c2726e03a67a6d3a6c51ae1005a
Usuario <Usuario@192.168.60.47>
2017-10-25 14:26:02 +0200

    FrontArticulo DVD

--- src/FrontArticulo.java
+++ src/FrontArticulo.java
@@ -21,6 +21,7 @@ public class FrontArticulo  extends JFrame{
 		setVisible(true);
 		this.setLocationRelativeTo(null);   
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	public void init() {
@@ -28,7 +29,7 @@ public class FrontArticulo  extends JFrame{
 		//posiciono el JPanel
 		setBounds(1000, 500, 1000, 1000);
 		panelPrincipal=new JPanel();
-		//panelPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panelPrincipal.setBorder(new EmptyBorder(5, 100, 5, 100));
 		
 		//this.setPanelPrincipal(panelPrincipal);
 		panelPrincipal.setLayout(new GridLayout(2, 2, 100, 30));
@@ -49,50 +50,50 @@ public class FrontArticulo  extends JFrame{
 		lblcd.setBounds(10, 20, 50, 50);
 		panelcd.add(lblcd);
 		
-		JLabel idCD = new JLabel(" idCD");
-		idCD.setBounds(500, 18, 50, 50);
-		panelcd.add(idCD);
-	
-		
-		JTextField textoId = new JTextField(10);
-		textoId.setBounds(425,53,200,25);
-		panelcd.add(textoId);
-		
+//		JLabel idCD = new JLabel(" idCD");
+//		idCD.setBounds(620, 18, 50, 50);
+//		panelcd.add(idCD);
+//	
+//		
+//		JTextField textoId = new JTextField(10);
+//		textoId.setBounds(545,53,200,25);
+//		panelcd.add(textoId);
+//		
 		JLabel tituloCD = new JLabel("Título");
-		tituloCD.setBounds(500, 68, 100, 50);	;
+		tituloCD.setBounds(620, 68, 100, 50);	;
 		panelcd.add(tituloCD);
 		
 		JTextField  textoTituloCD= new JTextField(10);
-		textoTituloCD.setBounds(425, 103, 200, 25);	
+		textoTituloCD.setBounds(545, 103, 200, 25);	
 		panelcd.add(textoTituloCD);	
 
 		
 		JLabel discografia = new JLabel("Discografia");
-		discografia.setBounds(490,118,100,50);
+		discografia.setBounds(610,118,100,50);
 		panelcd.add(discografia);
 		
 		JTextField textoDiscografia = new JTextField(10);
-		textoDiscografia.setBounds(425,153,200,25);
+		textoDiscografia.setBounds(545,153,200,25);
 		panelcd.add(textoDiscografia);
 		
 		JLabel stockCD = new JLabel("Stock");
-		stockCD.setBounds(500,168,50,50);
+		stockCD.setBounds(620,168,50,50);
 		panelcd.add(stockCD);
 		
 		JTextField textoStock = new JTextField(10);
-		textoStock.setBounds(425,203,200,25);
+		textoStock.setBounds(545,203,200,25);
 		panelcd.add(textoStock);
 		
 		JLabel cantante = new JLabel("Cantante");
-		cantante.setBounds(500,218,90,50);
+		cantante.setBounds(620,218,90,50);
 		panelcd.add(cantante);
 		
 		JTextField textoCantante = new JTextField(10);
-		textoCantante.setBounds(425,253,200,25);
+		textoCantante.setBounds(545,253,200,25);
 		panelcd.add(textoCantante);
 		
 		JButton anadirCD = new JButton("Añadir");
-		anadirCD.setBounds(480,283,100,20);
+		anadirCD.setBounds(600,283,100,20);
 		panelcd.add(anadirCD);
 		
 		
@@ -107,9 +108,47 @@ public class FrontArticulo  extends JFrame{
 		paneldvd.setLayout(null);
 		  
 		//etiqueta para la pestaña de dvd
-		JLabel lbldvd = new JLabel("Segunda dvd");
+		JLabel lbldvd = new JLabel("Añadir dvd");
 		lbldvd.setBounds(10, 11, 348, 14);
 		paneldvd.add(lbldvd);
+		
+		JLabel tituloDVD = new JLabel("Titulo Pelicula");
+		tituloDVD.setBounds(600, 18, 100,50);
+		paneldvd.add(tituloDVD);
+		
+		JTextField TextoTitulo = new JTextField(10);
+		TextoTitulo.setBounds(545, 53,200,25);
+		paneldvd.add(TextoTitulo);
+		
+		
+		JLabel director = new JLabel("Director");
+		tituloDVD.setBounds(600, 68, 100,50);
+		paneldvd.add(tituloDVD);
+		
+		JTextField TextoDirector = new JTextField(10);
+		TextoDirector.setBounds(545,103,200,25 );
+		paneldvd.add(TextoDirector);
+		
+		JLabel productor = new JLabel("Productora");
+		productor.setBounds(600, 118, 100,50);
+		paneldvd.add(productor);
+		
+		JTextField TextoProductor = new JTextField(10);
+		TextoProductor.setBounds(545,153,200,25 );
+		paneldvd.add(TextoProductor);
+		
+		JLabel stockDVD = new JLabel("Stock");
+		stockDVD.setBounds(600,168,100,50);
+		paneldvd.add(stockDVD);
+		
+		JTextField TextoStock = new JTextField(10);
+		TextoStock.setBounds(545,203,200,25);
+		paneldvd.add(TextoStock);
+			
+	
+		JButton anadir = new JButton("Añadir");
+		anadir.setBounds(600,233,25,25);
+		paneldvd.add(anadir);
 		 
 		//tercera pestaña
 		JPanel panelLibro = new JPanel();
--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		//Conexion conn = new Conexion();
 //		FrontAdmin admin = new FrontAdmin();
-<<<<<<< HEAD
-//		FrontArticulo articulo = new FrontArticulo();
+//<<<<<<< HEAD
+		FrontArticulo articulo = new FrontArticulo();
 		//FrontAlquilarCD alquilar = new FrontAlquilarCD();
 //		FrontAnadirCD f1 = new FrontAnadirCD();
-		FrontAnadirDVD f2 = new FrontAnadirDVD();
-		FrontAnadirLibro f3 = new FrontAnadirLibro();
+		//FrontAnadirDVD f2 = new FrontAnadirDVD();
+		//FrontAnadirLibro f3 = new FrontAnadirLibro();
 		//FrontLogin f4 = new FrontLogin();
-=======
-
-		FrontArticulo articulo = new FrontArticulo();
+//=======
+//
+		//FrontArticulo articulo = new FrontArticulo();
 	//	FrontAlquilarCD alquilar = new FrontAlquilarCD();
 		
 		
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural.git
+//>>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural.git
 		
 
 	}
