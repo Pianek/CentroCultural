1d420b0c4fa2e7afe97c1f2a86edaae995270536
Usuario <Usuario@DESKTOP-5DF5DQ2>
2017-10-26 09:09:38 +0200

    *

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Conexion conn = new Conexion();
-<<<<<<< HEAD
 //		FrontAdmin admin = new FrontAdmin();
 
 //		FrontArticulo articulo = new FrontArticulo();
-		//FrontAlquilarCD alquilar = new FrontAlquilarCD();
+		FrontAlquilarCD alquilar = new FrontAlquilarCD();
 //		FrontAnadirCD f1 = new FrontAnadirCD();
-		FrontAnadirDVD f2 = new FrontAnadirDVD();
+//		FrontAnadirDVD f2 = new FrontAnadirDVD();
 		
-
-=======
-<<<<<<< HEAD
-		FrontPrestamo f1 = new FrontPrestamo();
-
-		
-
-=======
-//		FrontAdmin admin = new FrontAdmin();
-
-//		FrontArticulo articulo = new FrontArticulo();
-		FrontAlquilarCD alquilarcd = new FrontAlquilarCD();
-//		FrontAlquilarDVD alquilardvd = new FrontAlquilarDVD();
-//		FrontAlquilarLibro alquilarlibro = new FrontAlquilarLibro();
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural.git
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural.git
 	}
 
 }
